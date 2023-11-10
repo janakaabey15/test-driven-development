2e1b3d79443c20389cfd2e5a18b468b45e7b3fd0
Janaka <janakaabey15@gmail.com>
2023-11-10 15:11:40 +0100

    Intial Commit

---
+++ src/main/java/com/janaka/isbntools/ValidateISBN.java
@@ -0,0 +1,4 @@
+package com.janaka.isbntools;
+
+public class ValidateISBN {
+}
---
+++ src/test/java/com/janaka/isbntools/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.janaka.isbntools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest(){
+
+
+    }
+}
