f02a2492749603fedd4acc1b5132dc06496987ee
Janaka <janakaabey15@gmail.com>
2023-11-14 15:12:30 +0100

    Adding 13 Digit isbn check

--- src/test/java/com/janaka/isbntools/ValidateISBNTools.java
+++ src/test/java/com/janaka/isbntools/ValidateISBNTools.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ValidateISBNTools {
 
    @Test
-    public void checkValidISBN(){
+    public void checkValid10DigitISBN(){
      ValidateISBN validateISBN = new ValidateISBN();
      boolean result = validateISBN.checkISBN("0140449116");
      assertTrue(result,"First value");
@@ -15,11 +15,50 @@ public class ValidateISBNTools {
      assertTrue(result,"Second Value");
    }
 
+   @Test
+   public void checkValid13DigitsISBN(){
+       ValidateISBN validateISBN = new ValidateISBN();
+       boolean result = validateISBN.checkISBN("9781853260087");
+       assertTrue(result,"First Value");
+       result = validateISBN.checkISBN("9781853267338");
+       assertTrue(result,"Second Value");
+
+   }
+   @Test
+   public void validWhenIsbn10DigitEndsWithX(){
+       ValidateISBN validateISBN = new ValidateISBN();
+       boolean result = validateISBN.checkISBN("012000030X");
+       assertTrue(result);
+   }
+
     @Test
-    public void checkAnInValidISBN(){
+    public void checkAnInValid10DigitISBN(){
         ValidateISBN validateISBN = new ValidateISBN();
         boolean result = validateISBN.checkISBN("0140449117");
         assertFalse(result);
     }
+    @Test
+    public void checkAnInValid13DigitISBN(){
+       ValidateISBN validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("9781853267334");
+        assertFalse(result);
 
+
+    }
+    @Test
+    public void nineDigitISBNAreNotAllowed(){
+     ValidateISBN validateISBN = new ValidateISBN();
+     assertThrows(NumberFormatException.class,
+             ()-> validateISBN.checkISBN("123456789"));
+
+    }
+
+    @Test
+    public void noneNumericIsbnNotAllowed(){
+       ValidateISBN validateISBN = new ValidateISBN();
+        assertThrows(NumberFormatException.class,
+                ()-> {
+            validateISBN.checkISBN("HelloWorld");
+                });
+    }
 }
